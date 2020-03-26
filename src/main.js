88470ec5fcf8f694b21b4e9e1aa4a0a378754f5b
yestares <13627970720@163.com>
2020-03-26 21:51:54 +0800

    first-add

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import App from "./App.vue";
 import router from "./router";
+import store from './store'
 
 Vue.use(ElementUI);
 import "./permission";
 Vue.config.productionTip = process.env.NODE_ENV === "production";
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount("#app");
